b8728ae842a9424d1e83304f3faacc48be6779b9
Taylor Monacelli <taylormonacelli@gmail.com>
2024-10-21 11:02:36 -0700

    Revert "error for exists"
    This reverts commit 029150159a71fe8c7f5a18f3734364500fd8ec38.

--- core/compare.go
+++ core/compare.go
@@ -1,14 +1,11 @@
 package core
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 )
-
-var ErrNoMatchingFiles = errors.New("no matching files found for comparison")
 
 type FileComparer struct {
 	SourceDir      string
@@ -31,7 +28,6 @@ func (fc *FileComparer) GenerateComparisonScript(outputFile string) error {
 	}
 
 	commands := []string{"#!/usr/bin/env bash\n"}
-	matchingFiles := false
 
 	for _, sourceFile := range sourceFiles {
 		relPath, err := filepath.Rel(fc.SourceDir, sourceFile)
@@ -47,7 +43,6 @@ func (fc *FileComparer) GenerateComparisonScript(outputFile string) error {
 
 		if fc.fileExists(targetFile) {
 			// Convert both sourceFile and targetFile to absolute paths
-			matchingFiles = true
 			absSourceFile, err := filepath.Abs(sourceFile)
 			if err != nil {
 				return err
@@ -60,10 +55,6 @@ func (fc *FileComparer) GenerateComparisonScript(outputFile string) error {
 			command := fmt.Sprintf("diff --unified --ignore-all-space %q %q", absSourceFile, absTargetFile)
 			commands = append(commands, command)
 		}
-	}
-
-	if !matchingFiles {
-		return ErrNoMatchingFiles
 	}
 
 	script := strings.Join(commands, "\n")
